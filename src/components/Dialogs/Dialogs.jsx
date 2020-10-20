1952d4a90b92fe5e0f0062fd9f05eaff249decfd
acknoilovewieti <kostya.turov@gmail.com>
2020-10-20 10:45:13 -0500

    Added dialogsElements

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -29,6 +29,15 @@ const Dialogs = (props) => {
         {id: 6, name: "Valera"}
     ];
 
+    let dialogsElements =[
+        <DialogItem name={dialogsData[0].name} id={dialogsData[0].id} />,
+        <DialogItem name={dialogsData[1].name} id={dialogsData[1].id} />,
+        <DialogItem name={dialogsData[2].name} id={dialogsData[2].id} />,
+        <DialogItem name={dialogsData[3].name} id={dialogsData[3].id} />,
+        <DialogItem name={dialogsData[4].name} id={dialogsData[4].id} />,
+        <DialogItem name={dialogsData[5].name} id={dialogsData[5].id} />
+    ];
+
     let messageData =[
         {id: 1, message: "Hi"},
         {id: 1, message: "Hi, how are you"},
@@ -40,12 +49,7 @@ const Dialogs = (props) => {
     return (
         <div className={s.dialogs}>
             <div className={s.dialogsItems}>
-                <DialogItem name={dialogsData[0].name} id={dialogsData[0].id} />
-                <DialogItem name={dialogsData[1].name} id={dialogsData[1].id} />
-                <DialogItem name={dialogsData[2].name} id={dialogsData[2].id} />
-                <DialogItem name={dialogsData[3].name} id={dialogsData[3].id} />
-                <DialogItem name={dialogsData[4].name} id={dialogsData[4].id} />
-                <DialogItem name={dialogsData[5].name} id={dialogsData[5].id} />
+                {dialogsElements}
             </div>
 
             <div className={s.messages}>
